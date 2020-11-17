0ee3e572e82ba6b197b0ded2232f677cbe92a4a2
daithemu <49351808+daithemu@users.noreply.github.com>
2020-11-16 21:03:00 -0700

    consulta completa

---
+++ src/app/services/student.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import {Student} from "../models/student";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentService {
+
+  private students: Student[] = new Array();
+
+  constructor() {
+    this.students.push({
+      name: 'Daniela Itzel Hernandez Muñoz',
+      controlnumber: '16400929',
+      active: true
+    });
+    this.students.push({
+      name: 'Pedro Pérez ',
+      controlnumber: '16400928',
+      active: true
+    });
+    this.students.push({
+      name: 'Daniela Itzel Hernandez Nuñez',
+      controlnumber: '16400927',
+      active: false
+    });
+   }
+
+   getStudents(): Student[]{
+     return this.students;
+   }
+
+   changeStatus(position: number): void{
+     this.students[position].active = !this.students[position].active;
+   }
+} 
