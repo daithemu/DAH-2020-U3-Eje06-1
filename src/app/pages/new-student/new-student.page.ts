53cb6380e9501999599917655784a2e88ddeb333
daniela itzel hernandez muñoz <daithernandezmu@ittepic.edu.mx>
2020-11-18 20:31:06 -0700

    Formularios (validar y guardar)

--- src/app/pages/new-student/new-student.page.ts
+++ src/app/pages/new-student/new-student.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { StudentService} from '../../services/student.service';
+
+import { FormGroup, FormBuilder, Validator, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-new-student',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewStudentPage implements OnInit {
 
-  constructor() { }
+  myForm: FormGroup;
+  submitted = false;
+
+  constructor(private studentService: StudentService, public fb: FormBuilder) { }
 
   ngOnInit() {
+    this.myForm = this.fb.group(
+      {
+        name: ['', Validators.compose([Validators.required, Validators.minLength(6)])],
+        controlNumber: ['', Validators.compose([Validators.required])]
+      }
+    );
+  }
+
+  saveStudent(){
+    this.submitted = true; //cambiar la bandera
+
+    if(this.myForm.valid){
+      this.studentService.newStudent({
+        //esto es lo uqe esta en el modelo
+        name: this.myForm.get('name').value,
+        controlnumber: this.myForm.get('controlNumber').value,
+        active: false
+      });
+    }
   }
 
 }
